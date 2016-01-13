998ee875f7fff2cb382055898ea7b8fddb18b9b1
unknown <rea@LT-REA.dk.sitecore.net>
2016-01-13 14:17:36 +1100

    value list now stored in chrome sync storage

--- NewTabLinker/assets/js/scripts.js
+++ NewTabLinker/assets/js/scripts.js
@@ -1,20 +1,54 @@
-﻿var newTabLinker = {
+﻿var ntlModel = [];
+
+var newTabLinker = {
     params: {
-        inputName: '#txtLink',
-        buttonName: '#btnAddLink'
+        inputName: "#txtLink",
+        buttonName: "#btnAddLink",
+        valuesList: ".values-list",
+        paramName: "ntlStorageObjet"
     },
 
-    init: function () {
-        alert('ntl-init');
+    bindAddButton: function () {
+        var _this = this;
+        $(_this.params.buttonName).click(function (e) {
+            var inputValue = $(_this.params.inputName).val();
+            if (!inputValue) {
+                alert("Please enter some text");
+                return;
+            }
 
-        $(this.params.buttonName).click(function (e) {
-            alert('button clicked');
+            ntlModel.push(inputValue);
+            var save = {};
+            save[_this.params.paramName] = ntlModel;
+            chrome.storage.sync.set(save, function () {
+                _this.outputExistingValue();
+            });
+        });
+    },
+
+    outputExistingValue: function () {
+        var _this = this;
+        var existingValuesList = $(_this.params.valuesList);
+        existingValuesList.html("");
+        $.each(ntlModel, function (idx, val) {
+            existingValuesList.append("<li>" + val + "</li>");
+        });
+    },
+
+    loadDataFromStorage: function() {
+        var _this = this;
+        chrome.storage.sync.get(_this.params.paramName, function (obj) {
+            if (obj[_this.params.paramName]) {
+                ntlModel = obj[_this.params.paramName];
+                _this.outputExistingValue();
+            }
         });
     }
 };
 
 (function ($) {
     $(document).ready(function () {
-        newTabLinker.init();
+        newTabLinker.loadDataFromStorage();
+        newTabLinker.bindAddButton();
     });
 }(jQuery));
