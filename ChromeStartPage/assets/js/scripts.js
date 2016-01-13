bd95e376f3e02ca2f2ca990638de1e5be94cd312
unknown <rea@LT-REA.dk.sitecore.net>
2016-01-13 11:41:58 +1100

    stubbed out js class and html form

---
+++ ChromeStartPage/assets/js/scripts.js
@@ -0,0 +1,20 @@
+﻿var newTabLinker = {
+    params: {
+        inputName: '#txtLink',
+        buttonName: '#btnAddLink'
+    },
+
+    init: function () {
+        alert('ntl-init');
+
+        $(this.params.buttonName).click(function (e) {
+            alert('button clicked');
+        });
+    }
+};
+
+(function ($) {
+    $(document).ready(function () {
+        newTabLinker.init();
+    });
+}(jQuery));
